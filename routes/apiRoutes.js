0eed7a458a24e1f005ec3f798e29640655e80e93
Machikon <106935371+Machikon@users.noreply.github.com>
2022-12-08 18:45:40 -0800

    updated Routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const fs =  require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+const router = require('express').Router;
+
+
+
+module.exports = app =>{
+
+// GET - read notes
+app.get('/api/notes', (req, res) =>{
+  res.sendFile(path.join(__dirname, '../db/db.json'));
+});
+
+// POST - add notes
+app.post('/api/notes', (req, res) => {
+  fs.writeFile('db/db.json').then((db) => res.json(JSON.parse(db)));
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNotes = {
+      title,
+      text,
+      id:uuidv4(),
+    };
+
+    readAndAppend(newNotes, 'db/db.json');
+    res.json(`Notes added successfully 🚀`);
+  } else {
+    res.error('Error in adding notes');
+  }
+});
+
+// POST read json file and return all saved notes
+// fb.post('/', (req, res) => {
+//   fs.writeFile('db/db.json'.JSON.stringify(db));
+//   res.json(db);
+// });
+
+}
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs =  require('fs');
+const notes = require('./db/db.json');
+
+
+
+
+const PORT = process.env.PORT || 3001;
+
+const apiRoutes = require('./routes/apiRoutes.js');
+const htmlRoutes = require('./routes/htmlRoutes.js');
+
+
+// Middleware for parsing JSON and urlencoded form data 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/',htmlRoutes);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
+
+
+
+
+
+
