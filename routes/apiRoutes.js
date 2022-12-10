ea3f2b0b5d28aaafbca929872bbd10f381b6e559
Machikon <106935371+Machikon@users.noreply.github.com>
2022-12-10 08:22:45 -0800

    fixed apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,37 +1,50 @@
 const path = require('path');
 const fs =  require('fs');
 const { v4: uuidv4 } = require('uuid');
+const { json } = require('express');
 
-const router = require('express').Router;
+const router = require('express').Router();
 
 
 
-module.exports = app =>{
+// module.exports = app =>{
 
 // GET - read notes
-app.get('/api/notes', (req, res) =>{
-  res.sendFile(path.join(__dirname, '../db/db.json'));
+router.get('/notes', (req, res) =>{
+  // res.readFile(path.join(__dirname, '../db/db.json'));
+  fs.readFile(path.join(__dirname, '../db/db.json'),"utf8",(err,data)=>{
+    const notes = JSON.parse(data)
+    res.json(notes)
+  })
 });
 
 // POST - add notes
-app.post('/api/notes', (req, res) => {
-  fs.writeFile('db/db.json').then((db) => res.json(JSON.parse(db)));
+router.post('/notes', (req, res) => {
+  fs.readFile(path.join(__dirname, '../db/db.json'),"utf8",(err,data)=>{
+    const notes = JSON.parse(data)
+    if (req.body) { 
+      const { title, text } = req.body;
+      const newNotes = {
+        title,
+        text,
+        id:uuidv4(),
+      };
+      notes.push(newNotes)
+      fs.writeFile('db/db.json',JSON.stringify(notes),(err,data)=>{
+        
+        // res.json(JSON.parse(data)); 
+        res.json(newNotes)
+      })
+    } else {
+      res.json('Error in adding notes');
+    }
+  })
+ 
 
-  const { title, text } = req.body;
 
-  if (req.body) {
-    const newNotes = {
-      title,
-      text,
-      id:uuidv4(),
-    };
-
-    readAndAppend(newNotes, 'db/db.json');
-    res.json(`Notes added successfully 🚀`);
-  } else {
-    res.error('Error in adding notes');
-  }
 });
+
+module.exports = router
 
 // POST read json file and return all saved notes
 // fb.post('/', (req, res) => {
@@ -39,4 +52,4 @@ app.post('/api/notes', (req, res) => {
 //   res.json(db);
 // });
 
-}
+
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,16 @@
 const router = require('express').Router();
 const path = require('path');
 
-module.exports = app =>{
+// module.exports = app =>{
 
 // GET - Route for notes
-app.get('/notes', (req, res) =>
+router.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '../public/notes.html'))
 );
 
 // GET - Route for index.html file
-app.get('*', (req, res) =>
+router.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '../public/index.html'))
 );
-};
+
+module.exports = router
